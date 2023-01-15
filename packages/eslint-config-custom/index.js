36e2216ab284021bd93ebfda676ad0235ff2df42
JeGwan <emperorvv@gmail.com>
2023-01-15 23:42:52 +0900

    :sparkles: Add eslint custom config package

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['prettier'],
+  rules: {
+    'no-console': 'warn',
+    'consistent-return': 'warn',
+    'no-unused-vars': 'warn',
+    'unused-imports/no-unused-imports': 'warn',
+  },
+}
