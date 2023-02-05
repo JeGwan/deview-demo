43d6193f8c42c31c8024d928340663aa0be2ca87
JeGwan <emperorvv@gmail.com>
2023-02-06 00:51:42 +0900

    :wrench: Add under components documents

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -4,7 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: path.resolve(__dirname, '../api/src/schema.gql'),
-  documents: './pages/**/*.tsx',
+  documents: ['./pages/**/*.{tsx,ts}', './components/**/*.{tsx,ts}'],
   ignoreNoDocuments: true,
   generates: {
     './lib/gql/': {
