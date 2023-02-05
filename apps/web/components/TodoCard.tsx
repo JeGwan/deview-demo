ac776f6089586715fbdfded6ed1432fe95fb2e4c
JeGwan <emperorvv@gmail.com>
2023-02-06 00:20:46 +0900

    :lipstick: Add TodoCard style

--- apps/web/components/TodoCard.tsx
+++ apps/web/components/TodoCard.tsx
@@ -1,14 +1,16 @@
 import { Todo } from 'lib/gql/graphql'
 
+import styles from './TodoCard.module.scss'
+
 interface Props {
   todo: Todo
 }
 
 const TodoCard = ({ todo }: Props) => {
   return (
-    <li>
-      <h1>{todo.title}</h1>
-      {todo.description && <div>{todo.description}</div>}
+    <li className={styles.todo_card}>
+      <h1 className={styles.todo_title}>{todo.title}</h1>
+      {todo.description && <div className={styles.todo_desc}>{todo.description}</div>}
     </li>
   )
 }
