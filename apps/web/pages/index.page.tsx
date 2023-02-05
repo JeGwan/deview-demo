c2cec7a0b14910f424bb7b9accc0372c88996f31
JeGwan <emperorvv@gmail.com>
2023-02-06 00:17:39 +0900

    :lipstick: Add index style

--- apps/web/pages/index.page.tsx
+++ apps/web/pages/index.page.tsx
@@ -3,6 +3,8 @@ import { useQuery } from '@apollo/client'
 import TodoCardList from 'components/TodoCardList'
 import { graphql } from 'lib/gql'
 
+import styles from './index.module.scss'
+
 const Todos = graphql(`
   query Todos {
     todos {
@@ -18,14 +20,13 @@ const Todos = graphql(`
 const Index = () => {
   const { data, loading, error } = useQuery(Todos)
 
-  if (loading) return <div>loading</div>
-  if (error) return <div>error occured</div>
+  if (loading) return <main className={styles.main}>loading</main>
+  if (error) return <main className={styles.main}>error occured</main>
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <main className={styles.main}>
       <TodoCardList todos={data.todos} />
-    </div>
+    </main>
   )
 }
 
