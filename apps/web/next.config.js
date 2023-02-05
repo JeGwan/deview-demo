87a354317753d3b8d02af41c70e819073cb91d91
JeGwan <emperorvv@gmail.com>
2023-02-05 23:59:39 +0900

    :wrench: Specify page extensions

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      type: 'json',
-      use: 'yaml-loader',
-    })
-
-    return config
-  },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
